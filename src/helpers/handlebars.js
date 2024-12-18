75cf6b2f02b0196669db9c8ea24f8f6e4bc48f9a
HUY DINH <dinhhuy574@gmail.com>
2024-12-19 06:31:42 +0700

    add convertDate

---
+++ src/helpers/handlebars.js
@@ -0,0 +1,47 @@
+const Handlebars = require('handlebars');
+
+module.exports = {
+    print: (a) => console.log(a),
+    sum: (a, b) => a + b,
+    isNotEqual: (a, b) => {
+        console.log(a);
+        console.log(b);
+    },
+    convertDate: (d) => {
+        var options = {
+            weekday: 'long',
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric',
+        };
+        var formatDate = new Date(d);
+        var date = formatDate.toLocaleDateString('vi-VN');
+        var time = formatDate.toLocaleTimeString('vi-VN');
+        return `${date} ${time}`;
+    },
+    sortable: (field, sort) => {
+        const sortType = field === sort.column ? sort.type : 'default';
+        const icons = {
+            default: 'oi oi-elevator',
+            asc: 'oi oi-sort-ascending',
+            desc: 'oi oi-sort-descending',
+        };
+
+        const types = {
+            default: 'asc',
+            asc: 'desc',
+            desc: 'asc',
+        };
+
+        const icon = icons[sortType];
+        const type = types[sort.type];
+
+        const href = Handlebars.escapeExpression(
+            `?_sort&column=${field}&type=${type}`,
+        );
+
+        const output = `<a href="${href}"><span class="${icon}"></span></a>`;
+
+        return new Handlebars.SafeString(output);
+    },
+};
