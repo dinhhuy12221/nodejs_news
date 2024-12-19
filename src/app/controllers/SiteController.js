6c39bb36e857119fd661cb97cfec88240b27b449
HUY DINH <dinhhuy574@gmail.com>
2024-12-19 09:53:07 +0700

    add slug function to get specific posts based on category

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,13 +1,23 @@
-const express = require('express');
-const Course = require('../models/Post');
+const Post = require('../models/Post');
 const { multipleMongooseToObject } = require('../../util/mongoose');
 class SiteController {
     // [GET] /
     index(req, res, next) {
-        Course.find()
-            .then((courses) => {
+        Post.find()
+            .then((posts) => {
                 res.render('home', {
-                    courses: multipleMongooseToObject(courses),
+                    posts: multipleMongooseToObject(posts),
+                });
+            })
+            .catch(next);
+    }
+
+    // [GET] /:slug
+    slug(req, res, next) {
+        Post.find({ category: req.params.slug })
+            .then((posts) => {
+                res.render('home', {
+                    posts: multipleMongooseToObject(posts),
                 });
             })
             .catch(next);
