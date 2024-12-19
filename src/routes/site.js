c0e5f72cc0931b860a4b699388a4c2bf054afbd0
HUY DINH <dinhhuy574@gmail.com>
2024-12-19 09:53:40 +0700

    add route /:slug

--- src/routes/site.js
+++ src/routes/site.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const siteController = require('../app/controllers/SiteController');
 
 router.get('/search', siteController.search);
+router.get('/:slug', siteController.slug);
 router.get('/', siteController.index);
 
 module.exports = router;
