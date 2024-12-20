19161c82c17ee0098c23808cfa5cd62fd0cec265
HUY DINH <dinhhuy574@gmail.com>
2024-12-20 16:23:55 +0700

    update function loginPost by adding cookie and session

--- src/app/controllers/LoginController.js
+++ src/app/controllers/LoginController.js
@@ -1,20 +1,35 @@
-const Account = require('../models/Account');
+const User = require('../models/User');
+const { createSession } = require('../../util/session');
 const {
     mongooseToObject,
     multipleMongooseToObject,
 } = require('../../util/mongoose');
+
 class LoginController {
     // [GET] /
     async loginGet(req, res) {
-        res.render('login');
+        res.render('login', {
+            isNotValid: req.query.isNotValid,
+        });
     }
 
     async loginPost(req, res, next) {
-        const account = await Account.findOne({
-            username: req.body.username,
+        await User.findOne({
+            email: req.body.email,
             password: req.body.password,
         })
-            .then(() => res.redirect('/'))
+            .then((user) => {
+                if (user) {
+                    const sessionId = createSession(mongooseToObject(user));
+                    res.setHeader(
+                        'Set-Cookie',
+                        `sessionId=${sessionId}; max-age=3600; httpOnly;`,
+                    );
+                    res.redirect('/me/stored/posts');
+                } else {
+                    res.status(401).redirect('login?isNotValid=true');
+                }
+            })
             .catch(next);
     }
 }
