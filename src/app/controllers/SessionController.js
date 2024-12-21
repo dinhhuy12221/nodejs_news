a9bcd41d2546dd6884db7a31e8a3166258d563c5
HUY DINH <dinhhuy574@gmail.com>
2024-12-21 10:23:35 +0700

    update loginPost

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+const { createSession } = require('../../util/session');
+const {
+    mongooseToObject,
+    multipleMongooseToObject,
+} = require('../../util/mongoose');
+const Session = require('../models/Session');
+
+class SessionController {
+    // [GET] /login
+    async loginGet(req, res) {
+        res.render('login', {
+            title: 'Trang đăng nhập',
+            layout: 'loginLayout',
+            isNotValid: req.query.isNotValid,
+        });
+    }
+
+    // [POST] /login
+    async loginPost(req, res, next) {
+        await User.findOne({
+            email: req.body.email,
+            password: req.body.password,
+        })
+            .then((user) => {
+                if (user) {
+                    const sessionId = createSession(user.userId);
+                    res.setHeader(
+                        'Set-Cookie',
+                        `sessionId=${sessionId}; max-age=3600; httpOnly;`,
+                    );
+                    res.redirect('/me/stored/posts');
+                } else {
+                    res.status(401).redirect('login?isNotValid=true');
+                }
+            })
+            .catch(next);
+    }
+
+    // [POST] /logout
+    async logout(req, res, next) {
+        await Session.deleteOne({ sessionId: req.cookies.sessionId })
+            .then(() => {
+                res.clearCookie('sessionId');
+                res.redirect('/');
+            })
+            .catch(next);
+    }
+}
+
+module.exports = new SessionController();
