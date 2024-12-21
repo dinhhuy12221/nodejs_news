a460190ae33b047dd8660fa221a5106f3d6d4be3
HUY DINH <dinhhuy574@gmail.com>
2024-12-21 10:22:13 +0700

    add userId property

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,13 +1,23 @@
 const mongoose = require('mongoose');
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    _id: { type: Number },
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    phone: { type: String },
-});
+const UserSchema = new Schema(
+    {
+        _id: { type: Number },
+        userId: { type: Number, required: true },
+        username: { type: String, required: true },
+        email: { type: String, required: true },
+        password: { type: String, required: true },
+        phone: { type: String },
+    },
+    {
+        _id: false,
+        timestamps: true,
+    },
+);
+
+UserSchema.plugin(AutoIncrement);
 
 module.exports = mongoose.model('User', UserSchema);
